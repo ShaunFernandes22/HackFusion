eaa048b171b548af183d99a36df18ae0202532a4
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-03-15 15:40:50 +0530

    fix: adding bcrypt for storing hashed password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -9,8 +11,10 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  emailVerified: {
-    type: Date,
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
   },
   image: {
     type: String,
@@ -26,5 +30,22 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+userSchema.methods.createHash = async function (plainTextPassword) {
+  // hashing user's salt and password with 10 iterations
+  const saltRounds = 10;
 
-module.exports = mongoose.model("User", userSchema);
+  // first method to generate a salt and then create hash
+  const salt = await bcrypt.genSalt(saltRounds);
+  return await bcrypt.hash(plainTextPassword, salt);
+
+  // second method - Or we can create salt and hash in a single method also
+  // return await bcrypt.hash(plainTextPassword, saltRounds);
+};
+
+// Validating the candidate password with stored hash and hash function
+userSchema.methods.validatePassword = async function (candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = { User };
