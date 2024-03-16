88bdf6d4530a330173aa166e8eba7d044265fe42
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-03-16 15:03:08 +0530

    style: minor fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Landing from './pages/Landing'
-import Form from './components/Form'
-import RoomPage from './pages/room/RoomPage'
-
-
+import { useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Form from "./components/Form";
+import RoomPage from "./pages/room/RoomPage";
+import "./App.css";
 function App() {
   return (
-    <BrowserRouter>
+    <div className='app'>
       <Routes>
-        <Route path="/" element={<Landing/>}/>
-        <Route path="/meeting" element={<Form/>}/>
-        <Route path="/room/:roomId" element={<RoomPage/>}/>
+        <Route path="/" element={<Landing />} />
+        <Route path="/meeting" element={<Form />} />
+        <Route path="/room/:roomId" element={<RoomPage />} />
       </Routes>
-    </BrowserRouter>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
