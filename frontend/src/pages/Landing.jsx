2f4ad1a5bf389066f30ed88fc9038d66b70471d2
Shaun Fernandes <shaunferns94@gmail.com>
2024-03-16 16:09:20 +0530

    minor style fix

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import About from "./About";
 
 const Landing = () => {
   return (
