199db4f6e2baa68af114e2804f98229992d9ca1b
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-03-16 16:37:04 +0530

    style: styling signup and signin

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,4 +1,4 @@
-export function InputBox({ label, placeholder, onChange }) {
+export function InputBox({ label, placeholder, onChange, type = "text" }) {
   return (
     <div>
       <div className="text-sm font-medium text-left py-2">{label}</div>
@@ -6,6 +6,7 @@ export function InputBox({ label, placeholder, onChange }) {
         placeholder={placeholder}
         className="w-full px-2 py-1 border rounded border-slate-200"
         onChange={onChange}
+        type={type}
       />
     </div>
   );
