454cada47e265899d8e7a02941e1798ad710fdc7
Shaun Fernandes <shaunferns94@gmail.com>
2024-03-16 15:25:59 +0530

    cached meet fn

--- frontend/src/pages/room/RoomPage.jsx
+++ frontend/src/pages/room/RoomPage.jsx
@@ -1,13 +1,15 @@
-import React, {useRef} from 'react'
+import React, {useEffect, useRef, useCallback} from 'react'
 import { useParams } from 'react-router-dom'
 import {ZegoUIKitPrebuilt} from '@zegocloud/zego-uikit-prebuilt'
 
 const RoomPage = () => {
     const {roomId} = useParams();
+    const videoContainerRef = useRef(null);
 
-    const userId = "sa1234"
+   
+    const userId = Date.now().toString();
     const userName = "sachin"
-    const myMeeting = async (element) => {
+    const myMeeting = useCallback (async (element) => {
         const appId = 1568361024;
         const serverSecret = '2591024f1ebf436b3aa07a8d86a2e518';
         const kitToken = ZegoUIKitPrebuilt.generateKitTokenForTest(appId, serverSecret, roomId, userId, userName);
@@ -18,10 +20,17 @@ const RoomPage = () => {
                 mode: ZegoUIKitPrebuilt.VideoConference,
             }
         });
-    }   
+    }, [roomId] );
+
+    useEffect(() => {
+        if (videoContainerRef.current) {
+            myMeeting(videoContainerRef.current); 
+        }
+    }, [myMeeting]);   
+
    return (
-    <div>
-        <div ref={myMeeting}/>
+    <div className = "top-0" style={{ width: '100%', height: '100vh',display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+        <div ref={videoContainerRef} style={{ width: '100%', height: '100vh' }} />
     </div>
   )
 }
