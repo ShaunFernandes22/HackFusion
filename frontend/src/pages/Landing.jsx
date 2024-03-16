ea77d823059bb2fcd0f91a9bd20aa0ece39ffe5d
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-03-17 03:56:40 +0530

    style: adding operation flow on landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import "./Landing.css";
+import Aos from "aos";
+import { useEffect } from "react";
+import OperateFlow from "../components/OperateFlow";
+
 
 const Landing = () => {
+  useEffect(() => {
+    Aos.init({ duration: 1000, once: true, easing: "ease" });
+  });
+
   return (
     <>
       <Hero />
+      <OperateFlow />
       <Footer />
     </>
   );
