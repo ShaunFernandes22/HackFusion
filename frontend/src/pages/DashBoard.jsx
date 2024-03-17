809fd154cd8273349e09990215e66436f71e4647
Shaun Fernandes <shaunferns94@gmail.com>
2024-03-17 19:03:49 +0530

    configured

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,24 +1,15 @@
-<<<<<<< Updated upstream
 import React from 'react'
 import HistoryInfo from '../components/HistoryInfo'
+import { DataTable } from "../components/Table";
+import Table2 from "../components/Table2";
 
 const DashBoard = () => {
   return (
     <>
         <HistoryInfo/>
         {/* <Tables/> */}
-=======
-import React from "react";
-import Charts from "../components/Charts";
-import { DataTable } from "../components/Table";
-import Table2 from "../components/Table2";
-const DashBoard = () => {
-  return (
-    <>
-      <Charts />
-      {/* <DataTable /> */}
-      <Table2 />
->>>>>>> Stashed changes
+        {/* <DataTable /> */}
+        <Table2 />
     </>
   );
 };
