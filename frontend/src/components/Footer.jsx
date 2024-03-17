3a617512c0af128b953eabc9409b885362758776
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-03-17 21:13:36 +0530

    style: styled footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-import {
-  FaDiscord,
-  FaLinkedin,
-  FaGithubSquare,
-  FaInstagram,
-  FaTwitterSquare,
-} from "react-icons/fa";
 
 const Footer = () => {
   return (
     <footer className="border-y border-gray-700 bg-gray-900">
-      <div className="max-w-[1240px] mx-auto py-6 px-4 grid lg:grid-cols-3 gap-8 text-gray-300">
+      <div className="max-w-[1240px] mx-auto py-6 px-4 grid lg:grid-cols-3 gap-8 text-gray-300 ">
         <div>
           <h1 className="w-full text-3xl font-bold italic text-[#00df9a]">
             EduQuest
@@ -19,48 +12,51 @@ const Footer = () => {
           <h4 className="text-base text-white">Daksh Jain</h4>
           <h4 className="text-base text-white">Naman Shah</h4>
           <h4 className="text-base text-white">Shaun Fernandes</h4>
-          <div className="flex justify-between md:w-[75%] my-6">
-            <FaLinkedin size={30} />
-            <FaInstagram size={30} />
-            <FaTwitterSquare size={30} />
-            <FaGithubSquare size={30} />
-            <FaDiscord size={30} />
-          </div>
         </div>
-        <div className="lg:col-span-2 flex justify-between mt-6">
+        <div className="lg:col-span-2 flex justify-between ">
           <div>
             <h6 className="font-medium text-gray-400">Solutions</h6>
             <ul>
-              <li className="py-2 text-sm">Analytics</li>
-              <li className="py-2 text-sm">Marketing</li>
-              <li className="py-2 text-sm">Commerce</li>
-              <li className="py-2 text-sm">Insights</li>
+              <li className="py-2 text-sm hover:text-[#00df9a] cursor-pointer">
+                Analytics
+              </li>
+              <li className="py-2 text-sm hover:text-[#00df9a] cursor-pointer">
+                Marketing
+              </li>
+
+              <li className="py-2 text-sm hover:text-[#00df9a] cursor-pointer">
+                Insights
+              </li>
             </ul>
           </div>
           <div>
             <h6 className="font-medium text-gray-400">Support</h6>
             <ul>
-              <li className="py-2 text-sm">Pricing</li>
-              <li className="py-2 text-sm">Documentation</li>
-              <li className="py-2 text-sm">Guides</li>
-              <li className="py-2 text-sm">API Status</li>
+              <li className="py-2 text-sm hover:text-[#00df9a] cursor-pointer">
+                Pricing
+              </li>
+              <li className="py-2 text-sm hover:text-[#00df9a] cursor-pointer">
+                <a href="https://documenter.getpostman.com/view/24438498/2sA2xny9wk">
+                  Documentation
+                </a>
+              </li>
+
+              <li className="py-2 text-sm hover:color-green hover:text-[#00df9a] cursor-pointer">
+                <a href="https://documenter.getpostman.com/view/24438498/2sA2xny9wk">
+                  API Status
+                </a>
+              </li>
             </ul>
           </div>
           <div>
             <h6 className="font-medium text-gray-400">Company</h6>
             <ul>
-              <li className="py-2 text-sm">Blog</li>
-              <li className="py-2 text-sm">Jobs</li>
-              <li className="py-2 text-sm">Press</li>
-              <li className="py-2 text-sm">Careers</li>
-            </ul>
-          </div>
-          <div>
-            <h6 className="font-medium text-gray-400">Legal</h6>
-            <ul>
-              <li className="py-2 text-sm">Claim</li>
-              <li className="py-2 text-sm">Policy</li>
-              <li className="py-2 text-sm">Terms</li>
+              <li className="py-2 text-sm hover:text-[#00df9a] cursor-pointer">
+                Blog
+              </li>
+              <li className="py-2 text-sm hover:text-[#00df9a] cursor-pointer">
+                Jobs
+              </li>
             </ul>
           </div>
         </div>
