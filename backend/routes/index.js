4a0de3fa7804165226c1c32f36438571d4c117a4
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2024-03-17 15:25:06 +0530

    fix: deleted unrequired files

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const userRouter = require("./user");
-const meetingRouter = require("./meeting");
-
-const versionRouter = express.Router();
-versionRouter.use("/user", userRouter);
-versionRouter.use("/meet", meetingRouter);
-module.exports = versionRouter;
